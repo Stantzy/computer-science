0376d9c7f77e321016899bf3072518b7506075fc
Igor Bazhev <bazhew@mail.ru>
2025-11-18 15:47:58 +0500

    Refactor sorting test classes

--- tests/algorithms/sorting/SortTest.java
+++ tests/algorithms/sorting/SortTest.java
@@ -1,6 +1,9 @@
 package tests.algorithms.sorting;
 
 public abstract class SortTest {
+    protected static int[] numbers = { 5, 4, 0, 2, 3, 1, 6 };
+    protected static final int[] expected = { 0, 1, 2, 3, 4, 5, 6 };
+
     protected static void checkResult(int[] numbers, int[] expected) {
         for(int i = 0; i < numbers.length; i++) {
             if(numbers[i] != expected[i]) {
--- tests/algorithms/sorting/bubble/BubbleSortTest.java
+++ tests/algorithms/sorting/bubble/BubbleSortTest.java
@@ -8,8 +8,7 @@ import static algorithms.sorting.bubble.BubbleSort.bubbleSort;
 
 public class BubbleSortTest extends SortTest {
     public static void main(String[] args) {
-        int[] numbers = { 5, 4, 0, 2, 3, 1, 6 };
-        int[] expected = { 0, 1, 2, 3, 4, 5, 6 };
+
 
         System.out.println(Arrays.toString(numbers));
         bubbleSort(numbers);
