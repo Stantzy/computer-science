bbf204e182c2659e4773cbb5b252c239a1ca87fb
Igor Bazhev <bazhew@mail.ru>
2025-11-28 13:44:16 +0500

    Implement direct address table and tests

---
+++ data_structures/hash_table/DirectAddressTable.java
@@ -0,0 +1,70 @@
+package data_structures.hash_table;
+
+public class DirectAddressTable<V> {
+    private static final int DEFAULT_CAPACITY = 32;
+
+    private final Object[] table;
+    private int size;
+
+    public DirectAddressTable() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public DirectAddressTable(int capacity) {
+        this.table = new Object[capacity];
+        this.size = 0;
+    }
+
+    public void put(int key, V value) {
+        if(value == null)
+            throw new IllegalArgumentException("Value can't be null");
+
+        checkKey(key);
+
+        if(table[key] == null)
+            size++;
+
+        table[key] = value;
+    }
+
+    @SuppressWarnings("unchecked")
+    public V get(int key) {
+        checkKey(key);
+        return (V) table[key];
+    }
+
+    @SuppressWarnings("unchecked")
+    public V remove(int key) {
+        checkKey(key);
+
+        V value = (V) table[key];
+
+        if(value != null) {
+            table[key] = null;
+            size--;
+        }
+
+        return value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Object[] toArray() {
+        return table.clone();
+    }
+
+    private void checkKey(int key) {
+        if(key >= table.length || key < 0) {
+            throw new IndexOutOfBoundsException(
+                    "The key = " + key + " is out of bounds of the table" +
+                    " with capacity = " + table.length
+            );
+        }
+    }
+}
---
+++ tests/data_structures/hash_table/DirectAddressTableTest.java
@@ -0,0 +1,109 @@
+package tests.data_structures.hash_table;
+
+import data_structures.hash_table.DirectAddressTable;
+
+public class DirectAddressTableTest {
+    public static void main(String[] args) {
+        testGet();
+        testPut();
+        testRemove();
+        testSizeAndIsEmpty();
+    }
+
+    private static void testPut() {
+        DirectAddressTable<Integer> table = new DirectAddressTable<>();
+
+        table.put(0, 10);
+        table.put(1, 20);
+        table.put(2, 30);
+
+        boolean exceptionThrown = false;
+        try {
+            table.put(-1, 1);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+            assert e.getMessage() != null : "Exception message must not be null";
+        }
+
+        assert exceptionThrown : "testPut: exception expected";
+
+        exceptionThrown = false;
+        try {
+            table.put(3, null);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+            assert e.getMessage() != null : "Exception message must not be null";
+        }
+
+        assert exceptionThrown : "testPut: exception expected";
+
+        exceptionThrown = false;
+        try {
+            table.put(1_000_000, null);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+            assert e.getMessage() != null :
+                    "testPut: exception message must not be null";
+        }
+
+        assert exceptionThrown : "testPut: exception expected";
+    }
+
+    private static void testGet() {
+        DirectAddressTable<Integer> table = new DirectAddressTable<>();
+
+        table.put(0, 10);
+        table.put(1, 20);
+        table.put(2, 30);
+
+        Integer value;
+
+        value = table.get(0);
+        assert value == 10 : "testGet: expected 10, got " + value;
+
+        value = table.get(1);
+        assert value == 20 : "testGet: expected 20, got " + value;
+
+        value = table.get(2);
+        assert value == 30 : "testGet: expected 30, got " + value;
+
+        boolean exceptionThrown = false;
+        try {
+            table.get(-1);
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+            assert e.getMessage() != null :
+                    "testGet: Exception message must not be null";
+        }
+
+        assert exceptionThrown : "testGet: exception expected";
+    }
+
+    private static void testRemove() {
+
+    }
+
+    private static void testSizeAndIsEmpty() {
+        DirectAddressTable<Integer> table = new DirectAddressTable<>();
+        int value;
+        boolean isEmpty;
+
+        value = table.size();
+        assert value == 0 :
+                "testSizeAndIsEmpty: expected 0, got " + value;
+
+        isEmpty = table.isEmpty();
+        assert isEmpty : "testSizeAndIsEmpty: expected true, got " + isEmpty;
+
+        table.put(0, 10);
+        table.put(1, 20);
+        table.put(2, 30);
+
+        value = table.size();
+        assert value == 3 :
+                "testSizeAndIsEmpty: expected 3, got " + value;
+
+        isEmpty = table.isEmpty();
+        assert !isEmpty : "testSizeAndIsEmpty: expected false, got " + isEmpty;
+    }
+}
