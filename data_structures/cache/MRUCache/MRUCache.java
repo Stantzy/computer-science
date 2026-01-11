636a874a5a9111311c995eb832a1ee115a10eb12
Igor Bazhev <bazhew@mail.ru>
2026-01-11 21:32:28 +0500

    Implement MRU Cache and tests

---
+++ data_structures/cache/MRUCache/MRUCache.java
@@ -0,0 +1,100 @@
+package data_structures.cache.MRUCache;
+
+import java.util.HashMap;
+
+// Most Recently Used Cache
+public class MRUCache<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private final HashMap<K, Node<K, V>> map;
+    private final int capacity;
+    private Node<K, V> head;
+    private Node<K, V> tail;
+    private int size;
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+        Node<K, V> prev;
+        Node(K k, V v) { this.key = k; this.value = v; }
+    }
+
+    public MRUCache(int capacity) {
+        if(capacity < 1)
+            capacity = DEFAULT_CAPACITY;
+
+        map = new HashMap<>();
+        this.capacity = capacity;
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public V get(K key) {
+        Node<K, V> node = map.get(key);
+        removeNode(node);
+        setHead(node);
+        return (node == null) ? null : node.value;
+    }
+
+    public void put(K key, V value) {
+        Node<K, V> node;
+        if(map.containsKey(key)) {
+            node = map.get(key);
+            node.value = value;
+            removeNode(node);
+            setHead(node);
+            map.put(key, node);
+            return;
+        }
+
+        if(size >= capacity) {
+            map.remove(head.key);
+            removeMostRecentlyUsed();
+            size--;
+        }
+
+        node = new Node<>(key, value);
+        setHead(node);
+        map.put(key, node);
+        size++;
+    }
+
+    private void setHead(Node<K, V> node) {
+        if(node == null)
+            return;
+
+        if(head != null)
+            head.next = node;
+
+        node.prev = head;
+        node.next = null;
+        head = node;
+
+        if(tail == null)
+            tail = head;
+    }
+
+    private void removeMostRecentlyUsed() {
+        removeNode(head);
+    }
+
+    private void removeNode(Node<K, V> node) {
+        if(node == null)
+            return;
+
+        if(node.prev != null)
+            node.prev.next = node.next;
+        else
+            tail = node.next;
+
+        if(node.next != null)
+            node.next.prev = node.prev;
+        else
+            head = node.prev;
+
+        node.next = null;
+        node.prev = null;
+    }
+}
---
+++ tests/data_structures/cache/MRUCacheTest.java
@@ -0,0 +1,39 @@
+package tests.data_structures.cache;
+
+
+import data_structures.cache.MRUCache.MRUCache;
+
+public class MRUCacheTest {
+    public static void main(String[] args) {
+        testGet();
+        testPut();
+    }
+
+    private static void testGet() {
+        MRUCache<Integer, String> cache = new MRUCache<>(3);
+
+        cache.put(1, "A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+
+        assert cache.get(1).equals("A");
+        assert cache.get(2).equals("B");
+        assert cache.get(3).equals("C");
+        assert cache.get(4) == null;
+    }
+
+    private static void testPut() {
+        MRUCache<Integer, String> cache = new MRUCache<>(3);
+
+        cache.put(1, "A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+
+        cache.get(1);
+
+        cache.put(4, "D");
+
+        assert cache.get(1) == null;
+        assert cache.get(4).equals("D");
+    }
+}
