4a45fc44a0d97d191433cffd250d541ca251a7ab
Igor Bazhev <bazhew@mail.ru>
2026-01-11 18:17:56 +0500

    Implement LIFO Cache and tests

---
+++ data_structures/cache/LIFOCache/LIFOCache.java
@@ -0,0 +1,70 @@
+package data_structures.cache.LIFOCache;
+
+import java.util.HashMap;
+
+public class LIFOCache<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private final HashMap<K, V> map;
+    private Node<K> top;
+    private final int capacity;
+    private int size;
+
+    private static class Node<K> {
+        K key;
+        Node<K> next;
+
+        Node(K key) {
+            this.key = key;
+            next = null;
+        }
+    }
+
+    public LIFOCache() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public LIFOCache(int capacity) {
+        if(capacity < 1)
+            capacity = DEFAULT_CAPACITY;
+
+        map = new HashMap<>();
+        top = null;
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    public void put(K key, V value) {
+        if(map.containsKey(key)) {
+            map.put(key, value);
+            return;
+        }
+
+        if(size >= capacity) {
+            map.remove(top.key);
+            removeFromTop();
+            size--;
+        }
+
+        Node<K> node = new Node<>(key);
+        pushOnTop(node);
+        map.put(key, value);
+        size++;
+    }
+
+    private void pushOnTop(Node<K> node) {
+        node.next = top;
+        top = node;
+    }
+
+    private void removeFromTop() {
+        if(top == null)
+            return;
+
+        top = top.next;
+    }
+}
---
+++ tests/data_structures/cache/LIFOCacheTest.java
@@ -0,0 +1,35 @@
+package tests.data_structures.cache;
+
+import data_structures.cache.LIFOCache.LIFOCache;
+
+public class LIFOCacheTest {
+    public static void main(String[] args) {
+        testGet();
+        testPut();
+    }
+
+    private static void testGet() {
+        LIFOCache<Integer, String> cache = new LIFOCache<>(3);
+
+        cache.put(1, "A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+
+        assert cache.get(1).equals("A");
+        assert cache.get(2).equals("B");
+        assert cache.get(3).equals("C");
+        assert cache.get(4) == null;
+    }
+
+    private static void testPut() {
+        LIFOCache<Integer, String> cache = new LIFOCache<>(3);
+
+        cache.put(1, "A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+        cache.put(4, "D");
+
+        assert cache.get(3) == null;
+        assert cache.get(4).equals("D");
+    }
+}
